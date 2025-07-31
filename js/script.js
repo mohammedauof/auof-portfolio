500e231a420ca490ea889143529c56ed478ed1c8
Mohammed Auof <mohammedauof15@gmail.com>
2025-07-31 22:58:12 +0530

    Favicon, Back to top button

--- js/script.js
+++ js/script.js
@@ -15,3 +15,18 @@ function rotateTagline() {
 
 setInterval(rotateTagline, 3000); // Change tagline every 3 seconds
 
+const backToTopBtn = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    backToTopBtn.classList.add("show");
+  } else {
+    backToTopBtn.classList.remove("show");
+  }
+};
+
+
+backToTopBtn.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
