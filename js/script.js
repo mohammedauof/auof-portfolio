fa0f0dacd89340a62da093ed2f59dd5d252e3cce
Mohammed Auof <mohammedauof15@gmail.com>
2025-07-31 20:07:43 +0530

    First copy of portfolio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+const taglines = [
+  "Cybersecurity Enthusiast",
+  "Frontend Developer in Progress",
+  "Loves Clean UI & Ethical Hacking",
+  "Building Secure and Beautiful Web Experiences"
+];
+
+let taglineIndex = 0;
+const taglineElement = document.getElementById("changing-tagline");
+
+function rotateTagline() {
+  taglineIndex = (taglineIndex + 1) % taglines.length;
+  taglineElement.textContent = taglines[taglineIndex];
+}
+
+setInterval(rotateTagline, 3000); // Change tagline every 3 seconds
 
